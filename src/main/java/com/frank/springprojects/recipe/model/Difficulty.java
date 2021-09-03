937cb1db798cf7dfa73150012c5d190fbd1d272d
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-03 18:13:21 -0500

    Added more model entities.

---
+++ src/main/java/com/frank/springprojects/recipe/model/Difficulty.java
@@ -0,0 +1,5 @@
+package com.frank.springprojects.recipe.model;
+
+public enum Difficulty {
+    EASY, MDDERATE, HARD
+}
--- src/main/java/com/frank/springprojects/recipe/model/Recipe.java
+++ src/main/java/com/frank/springprojects/recipe/model/Recipe.java
@@ -2,10 +2,15 @@ package com.frank.springprojects.recipe.model;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import java.util.Set;
@@ -24,15 +29,24 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO add private Difficulty difficulty
+
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Notes notes;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -106,11 +120,35 @@ public class Recipe {
         this.image = image;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public Notes getNotes() {
         return notes;
     }
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
